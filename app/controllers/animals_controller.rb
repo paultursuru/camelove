33e26cc6cfbd0936184e60931cfe60e5171777dd
paul lahana <paul.lahana@gmail.com>
2019-11-18 16:51:36 +0100

    ok but not la view

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,2 +1,11 @@
 class AnimalsController < ApplicationController
+  def show
+    @animal = Animal.find(animal_params)
+  end
+
+  private
+
+  def animal_params
+    params.require(:animal).permit(:name, :age, :city, :price, :photo, :description, :breed, :colour)
+  end
 end
