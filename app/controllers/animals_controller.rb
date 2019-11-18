094fb4f7e0ca969a18bf6133692f9868dc534865
Cecile Rougnaux <cecile_rougnaux@hotmail.com>
2019-11-18 17:48:44 +0100

    new and create actions added

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,9 +1,25 @@
 class AnimalsController < ApplicationController
   def index
     @animals = Animal.all
-    
+  end
+
   def show
     @animal = Animal.find(animal_params)
+  end
+
+  def new
+    @animal = Animal.new
+  end
+
+  def create
+    @animal = Animal.new(animal_params)
+    @animal.user = current_user
+
+    if @animal.save
+      redirect_to animals_path
+    else
+      render :new
+    end
   end
 
   private
