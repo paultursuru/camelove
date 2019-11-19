5dfb8a382640508ed740d68561be7d3ce9d1fd98
Mathias Thibaud <math.thibaud@gmail.com>
2019-11-19 10:41:29 +0100

    added booking model route and controller

---
+++ db/migrate/20191119092849_create_bookings.rb
@@ -0,0 +1,14 @@
+class CreateBookings < ActiveRecord::Migration[5.2]
+  def change
+    create_table :bookings do |t|
+      t.references :animal, foreign_key: true
+      t.references :user, foreign_key: true
+      t.date :date_in
+      t.date :date_out
+      t.integer :review_rating
+      t.text :review_description
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_18_145800) do
+ActiveRecord::Schema.define(version: 2019_11_19_092849) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,19 @@ ActiveRecord::Schema.define(version: 2019_11_18_145800) do
     t.index ["user_id"], name: "index_animals_on_user_id"
   end
 
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "animal_id"
+    t.bigint "user_id"
+    t.date "date_in"
+    t.date "date_out"
+    t.integer "review_rating"
+    t.text "review_description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["animal_id"], name: "index_bookings_on_animal_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -43,4 +56,6 @@ ActiveRecord::Schema.define(version: 2019_11_18_145800) do
   end
 
   add_foreign_key "animals", "users"
+  add_foreign_key "bookings", "animals"
+  add_foreign_key "bookings", "users"
 end
