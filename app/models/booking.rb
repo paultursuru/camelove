af2c19a2a1aa13b2b593ee43f38acd7cd89a172c
Astrid de Boysson <astriddeboysson@gmail.com>
2019-11-19 12:06:47 +0100

    edit and update

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,5 @@ class Booking < ApplicationRecord
   belongs_to :user
   validates :date_in, presence: true
   validates :date_out, presence: true
-  validates :review_rating, presence: true, inclusion: { in: [1, 2, 3, 4, 5] }
-  validates :review_description, presence: true
+  validates :review_rating, inclusion: { in: [1, 2, 3, 4, 5] }
 end
