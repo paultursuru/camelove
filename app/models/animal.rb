339c05252a8a0b66aab5882e78bce024ef495e20
paul lahana <paul.lahana@gmail.com>
2019-11-20 12:40:59 +0100

    seeds are on

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,7 +1,7 @@
 class Animal < ApplicationRecord
   has_many :bookings
   belongs_to :user
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
   validates :breed, presence: true
   validates :age, presence: true
   validates :colour, presence: true
