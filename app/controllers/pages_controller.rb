580300335389dc66695cefa3c6e6e656b7ef41cc
Mathias Thibaud <math.thibaud@gmail.com>
2019-11-20 15:06:46 +0100

    done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,21 +1,12 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
-  before_action :get_animals, only: :dashboard
   def home
     @animals = Animal.all.sample(6)
   end
 
   def dashboard
-    @animal = Animal.new
+    @animals = current_user.animals # Animal.where(user: current_user)
+    # @animal = Animal.new
     @bookings = current_user.bookings
-  end
-
-  private
-  def get_animals
-    @animals = Animal.all
-  end
-
-  def get_bookings
-    @bookings = Booking.all
   end
 end
